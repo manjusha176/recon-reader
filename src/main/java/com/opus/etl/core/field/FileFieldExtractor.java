cfd89cc36929484fa6ffa3cd76bfa2e16c70ac70
yranjana21 <yranjana21@gmail.com>
2018-11-28 15:31:57 +0530

    Changes done for CSV Field extractor.

--- src/main/java/com/opus/etl/core/field/FileFieldExtractor.java
+++ src/main/java/com/opus/etl/core/field/FileFieldExtractor.java
@@ -1,7 +1,6 @@
 package com.opus.etl.core.field;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -11,38 +10,32 @@ import com.opus.etl.core.dto.ConfigDTO;
 import com.opus.etl.core.dto.Field;
 
 public class FileFieldExtractor implements IFieldExtractor {
-	private static final char DEFAULT_QUOTE = '"';
 
 	public Map<Integer, Map<String, String>> extractFields(ConfigDTO config, String record) {
-		int countDelimiter = 0;
 
 		List<String> headerList = getHeaderList(config);
 
 		String delimiter = config.getFieldSeperator();
-		char[] delimiterChar = delimiter.toCharArray();
+		char[] delimiterChar = delimiter.toCharArray(); //TODO convert delimiter datatype from String to char
 
-		for (int i = 0; i < record.length(); i++){
-			if (record.charAt(i) == delimiterChar[0]){
-				countDelimiter++;
-			}
+		ArrayList<String> words = new ArrayList<>();
+		boolean notInsideComma = true;
+		int start = 0;
+		for (int i = 0; i < record.length() - 1; i++){
+			if (record.charAt(i) == delimiterChar[0] && notInsideComma){
+				words.add(record.substring(start, i));
+				start = i + 1;
+			} else if (record.charAt(i) == '"') notInsideComma = !notInsideComma;
 		}
 
-		List<String> fieldList = new ArrayList<>(Arrays.asList(record.split(delimiter)));
-
-		if (countDelimiter != headerList.size() - 1){
-
-			System.out.println("Special case");
-			CSVFieldValidator csvValidator = new CSVFieldValidator();
-			fieldList = csvValidator.parseLine(record, delimiterChar[0], DEFAULT_QUOTE);
-
-		}
+		words.add(record.substring(start));
+		System.out.println(record + "---->" + words);
 
 		// set header and value in map
-		Map<String, String> fieldMap = getFieldMap(headerList, fieldList);
+		Map<String, String> fieldMap = getFieldMap(headerList, words);
 
 		// iterating map of key/value
 		Iterator<Map.Entry<String, String>> entries = fieldMap.entrySet().iterator();
-
 		while (entries.hasNext()){
 			Map.Entry<String, String> entry = entries.next();
 			System.out.println("Key = " + entry.getKey() + ", \t Value = " + entry.getValue());
@@ -74,4 +67,25 @@ public class FileFieldExtractor implements IFieldExtractor {
 		return headerList;
 	}
 
+	public static void main(String args[]) {
+		FileFieldExtractor validator = new FileFieldExtractor();
+		ConfigDTO c1 = new ConfigDTO();
+		c1.setFieldSeperator(",");
+		List<Field> fieldList = new ArrayList<>();
+		Field f1 = new Field("id", "Type", 3, 4);
+		Field f2 = new Field("Name", "Type", 3, 4);
+		Field f3 = new Field("Address", "Type", 3, 4);
+		Field f4 = new Field("Amount", "Type", 3, 4);
+		fieldList.add(f1);
+		fieldList.add(f2);
+		fieldList.add(f3);
+		fieldList.add(f4);
+		c1.setFieldList(fieldList);
+		validator.extractFields(c1, "\"1\",\"US\",\"United States\"");
+		validator.extractFields(c1, "\"aaa\",\"b,bb\",\"ccc\"");
+		validator.extractFields(c1, "\"aaa\",\"b\"\"bb\",\"ccc\"");
+		validator.extractFields(c1, "\"aaa\",\"b\"\"bb\",\"ccc\"");
+		validator.extractFields(c1, "11,AU,Aus\"\"tralia");
+
+	}
 }
